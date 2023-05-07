41458cbffe3b5e802e69029d7ed1b0b305fa28ea
EncodedMatt <2213277@slu.edu.ph>
2023-05-07 13:34:10 +0800

    add message dialog

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,8 +7,4 @@ public class Calculator {
     public double calculateSquare(double side){
         return Math.pow(side,2);
     }
-
-    public double calculateTriangle(double base,double height){
-        return (height*base)/2;
-    }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -4,13 +4,11 @@ import java.util.Objects;
 
 public class GUI {
     JFrame frame = new JFrame();
-    String[] shapeArr = {"Square", "Circle"};
+    String[] shapeArr = {"Side of a Square", "Radius of a Circle"};
     JComboBox<String> shapes = new JComboBox<>(shapeArr);
     JTextField tf = new JTextField();
-    JLabel input = new JLabel();
-    JLabel outputName = new JLabel("Result: ");
-    JLabel outputValue = new JLabel("Dito lalabas yung sagot");
-    JPanel pane = new JPanel();
+
+    JPanel pane = new JPanel(new GridBagLayout());
     JButton button = new JButton("Calculate");
 
     public GUI(){
@@ -20,27 +18,32 @@ public class GUI {
             String text = tf.getText();
             try{
                 //if combo box is square, calculate square
-                if(Objects.equals(shapes.getSelectedItem(), "Square"))
-                    outputValue.setText(String.valueOf(calc.calculateSquare(Double.parseDouble(text)))); //change the text in outputValue label
+                if(Objects.equals(shapes.getSelectedItem(), "Side of a Square"))
+                    JOptionPane.showMessageDialog(null, "The side of the square is: " + calc.calculateSquare(Double.parseDouble(text)), "Side of a Square", JOptionPane.INFORMATION_MESSAGE);
                 //else if combox box is circle, calculate circle
-                else if(Objects.equals(shapes.getSelectedItem(), "Circle"))
-                    outputValue.setText(String.valueOf(calc.calculateCircle(Double.parseDouble(text)))); //change the text in outputValue label
+                else if(Objects.equals(shapes.getSelectedItem(), "Radius of a Circle"))
+                    JOptionPane.showMessageDialog(null, "The radius of a circle is: " + calc.calculateCircle(Double.parseDouble(text)), "Radius of a Circle", JOptionPane.INFORMATION_MESSAGE);
             }catch (NumberFormatException exception){
-                outputValue.setText("Enter numbers only");
+                JOptionPane.showMessageDialog(null, "Enter numbers only", "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
 
-        pane.add(shapes);
-        pane.add(input);
-        pane.add(tf);
-        pane.add(button);
-        pane.add(outputName);
-        pane.add(outputValue);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx=0;
+        gbc.gridy=0;
+        pane.add(shapes,gbc);
+        gbc.gridx=1;
+        gbc.gridy=0;
+        pane.add(tf,gbc);
+        gbc.gridx=2;
+        gbc.gridy=0;
+        pane.add(button,gbc);
 
         frame.add(pane);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setPreferredSize(new Dimension(400,100));
         frame.pack();
         frame.setVisible(true);
 
